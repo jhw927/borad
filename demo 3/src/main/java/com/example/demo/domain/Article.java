f5a71ecec2dcc32d1971701889ef328c3c432a27
JoHyunwoo <jhw3874@gmail.com>
2023-04-25 01:52:52 +0900

    #8 - 데이터베이스 접근 로직 생성

--- demo 3/src/main/java/com/example/demo/domain/Article.java
+++ demo 3/src/main/java/com/example/demo/domain/Article.java
@@ -1,18 +1,73 @@
 package com.example.demo.domain;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
 
 import java.time.LocalDateTime;
+import java.util.Objects;
 
+@Getter
+@ToString
+@Table(indexes = {
+        @Index(columnList = "title"),
+        @Index(columnList = "hashtag"),
+        @Index(columnList = "createdAt"),
+        @Index(columnList = "createdBy"),
+})
 @Entity
+@Setter
+@NoArgsConstructor
 public class Article {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(nullable = false)
     private String title;
+    @Column(nullable = false, length = 10000)
+    private String content;
+    @Column(nullable = false)
     private String hashtag;
 
     //metadata
+    @CreatedDate
     private LocalDateTime createdAt;
+    @CreatedBy
     private String createdBy;
+    @LastModifiedDate
     private LocalDateTime modifyAt;
+    @LastModifiedBy
     private String modifiedBy;
+
+
+    // 메타데이터는 뺸 본문에서 사용하는 데이터만 생성자로 받는다.
+    private Article(String title, String content, String hashtag) {
+        this.title = title;
+        this.content = content;
+        this.hashtag = hashtag;
+    }
+
+    // 팩토리 매서드 생성
+    public static Article of(String title, String content, String hashtag) {
+        return new Article(title, content, hashtag);
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Article article)) return false;
+        return id != null && id.equals(article.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
